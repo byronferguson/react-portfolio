e57de1a3860c4d8365927ea515c304e425b4e0da
Byron Ferguson <ferguson.bs@gmail.com>
2020-10-02 04:12:40 -0500

    tweaking nav color

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -26,7 +26,7 @@ export default function Nav({ pages, currentPage, setCurrentPage }) {
               {pages.map((page) => (
                 <button
                   onClick={() => navigateTo(page.slug)}
-                  className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                  className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-600 hover:text-gray-800 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:border-gray-300 transition duration-150 ease-in-out"
                   key={page.slug}
                 >
                   {page.title}
