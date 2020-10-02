5b335e14672dafa56562578c10dfe1b4c0559f6d
Byron Ferguson <ferguson.bs@gmail.com>
2020-10-02 04:05:08 -0500

    Updates

---
+++ precache-manifest.ff2ccf86a01ed6c9573c1ed1939b2787.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68c46cc2aab415847547d5ac137f1c4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d939798968887b372b5d",
+    "url": "/react-portfolio/static/css/main.973c93cc.chunk.css"
+  },
+  {
+    "revision": "c10fd9db6d5e79095733",
+    "url": "/react-portfolio/static/js/2.2f659110.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.2f659110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d939798968887b372b5d",
+    "url": "/react-portfolio/static/js/main.86c647cf.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "3e297a72a53c6eeb36f046a7f2a64715",
+    "url": "/react-portfolio/static/media/profile.3e297a72.jpg"
+  }
+]);
